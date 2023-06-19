719a82d3e1edb51a3fed548f98aac89f730cad8a
Gavincko227 <37636249+Gavincko227@users.noreply.github.com>
2023-06-19 14:01:48 -0400

    Update streamlit_app.py
    added variables to record which fruits were selected
    added another variable to oull the fruits that were selected from the main fruits list and store them in another variable
    then we put that fruits we want to show into the streamlit dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,14 +17,18 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 
-# Let's put a pick list here so they can pick the fruit they want to include 
+# Let's put a pick list here so they can pick the fruit they want to include - lesson 3 early part of the lesson
 #streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
 #this way we will be able to pre populate 
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
+fruits_selected=streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
+fruits_to_show = my_fruit_list.loc[fruits_selected]
+
+#display table on the page -- earlier step displaying all the fruits
+#streamlit.dataframe(my_fruit_list)
 
 
+#display the table on the page - only selected fruits
+streamlit.dataframe(fruits_to_show)
 
-#display table on the page
-streamlit.dataframe(my_fruit_list)
 

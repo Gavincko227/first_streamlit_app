cb2b1921af9916790327e61643b47ba007fe7e2a
Gavincko227 <37636249+Gavincko227@users.noreply.github.com>
2023-06-20 10:08:22 -0400

    Update streamlit_app.py
    Fixed typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,7 +34,7 @@ streamlit.dataframe(fruits_to_show)
 
 #NEW SECTION to display fruityvice api response
 streamlit.header('Fruityvice Fruit Advice!')
-fruit_choice = streamlit.text_input('What fruit would yo like information about?', 'Kiwi')#kiwi is the default fruit
+fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')#kiwi is the default fruit
 streamlit.write('the user entered', fruit_choice)
 
 

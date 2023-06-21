58ff03acdea9b7bc688f7959193998a7cc0d2bbd
Gavincko227 <37636249+Gavincko227@users.noreply.github.com>
2023-06-21 03:20:09 -0400

    Update streamlit_app.py
    quering data from the snowflake connector

--- streamlit_app.py
+++ streamlit_app.py
@@ -53,7 +53,14 @@ streamlit.dataframe(fruityvice_normalized)
 import snowflake.connector
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
-my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()") #getting the user and current_account
+"""my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()") #getting the user and current_account through SQL 
 my_data_row = my_cur.fetchone()
 streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)"""
+
+
+#querying data from snowflakeconnector
+my_cur.execute("select * from fruit_load_list")
+my_data_row = my_cur.fetchone()
+streamlit.text("The fruit load list contains:")
 streamlit.text(my_data_row)
